ffb71033ef546df8b131bd4f2df9f4098abaee4f
Siddhant <sidmee08@gmail.com>
2023-12-22 18:06:12 +0530

    new class

---
+++ src/basics_03_16Dec/Lab050.java
@@ -0,0 +1,17 @@
+package src.basics_03_16Dec;
+
+public class Lab050 {
+
+    public static void main(String[] args) {
+        byte number = 10;
+        int num2 =12;
+
+        if(num2%number==0){
+
+            System.out.println("number is divisible");
+        }
+        else {
+            System.out.println("try another number");
+        }
+    }
+}
